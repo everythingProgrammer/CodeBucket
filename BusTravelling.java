80a87ce8e48d6174b25ba98b0e312c66238f8a75
Abhinav Rana <ranaabhinav50@gmail.com>
2020-11-04 21:01:50 +0530

    .

--- BusTravelling.java
+++ BusTravelling.java
@@ -1,4 +1,33 @@
-
+import java.util.*;
 public class BusTravelling {
-
+	
+	
+	
+	public static void main(String args[]) {
+		Scanner sr = new Scanner(System.in);
+		int q = sr.nextInt();
+		int dis[][] = new int[q][2];
+		
+		for(int i = 0 ;i<q; i++) {
+			for(int j = 0 ;j<2 ; j++) {
+				dis[i][j] = sr.nextInt();
+			}
+		}
+		
+		int dist = dis[0][1] - dis[0][0];
+		for(int i =1 ; i<q; i++) {
+			for(int j = 0 ; j< 2 ; j++) {
+				int substract = 0 ;
+				if(dis[i][0] < dis[i-1][1]) {
+					substract = dis[i][1] - dis[i][0];
+				}
+				dist = dist - substract ; 
+				dist = dist +(dis[i][1] - dis[i][0]);
+			}
+		}
+		System.out.println(dist);
+	}
+	
+	
+	
 }
