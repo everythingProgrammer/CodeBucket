03ac915f070414c69d1f058193af85a1e0f759fc
Abhinav Rana <ranaabhinav50@gmail.com>
2020-11-04 19:31:19 +0530

    .

--- CodeEncyptedByBOB.java
+++ CodeEncyptedByBOB.java
@@ -1,4 +1,17 @@
 
 public class CodeEncyptedByBOB {
-
+	
+	
+	public static void main(String arsg[]) {
+		int S = 2;
+		int N = 3;
+		int M = 4;
+		
+		int enc =   (int) ((Math.pow((Math.pow(S, N)%10), M))%1000000007);
+		
+		System.out.println(enc);
+		
+	}
+	
+	
 }
