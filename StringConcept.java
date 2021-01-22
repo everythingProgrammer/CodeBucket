42a7f8f683dd50a2705107324578c8285052c2db
Abhinav Rana <ranaabhinav50@gmail.com>
2021-01-22 21:01:40 +0530

    Create StringConcept.java

---
+++ StringConcept.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class StringConcept {
+	
+	public static void display1(int a) {
+		System.out.println("int display1");
+	}
+	
+	public static void display1() {
+		System.out.println(" display");
+	}
+	
+	public void display12() {
+		
+	}
+	public void display12(int a) {
+		System.out.println(" ind display12");
+	}
+	
+	public static void main(String args[]) {
+		
+		
+		String a = "Abhinav";
+		String b = "Abhinav";
+		
+		System.out.println(a== b);
+	}
+	
+	
+}
