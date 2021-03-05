b2de4df2a3220312b5b2a7806f9d1cfb3d8adb9f
Abhinav Rana <ranaabhinav50@gmail.com>
2021-03-05 17:54:29 +0530

    Create PowerThroughRecursion.java

---
+++ PowerThroughRecursion.java
@@ -0,0 +1,18 @@
+
+public class PowerThroughRecursion {
+	
+	
+	
+	public static int power(int num , int pow) {
+		if(pow == 1) {
+			return num;
+		}
+		return num*power(num, pow-1);
+	}
+	
+	public static void main(String args[]) {
+		System.out.println(power(3, 3));
+	}
+	
+	
+}
