e2cd4692b72771fd86503eb6c9fe6dcc25776403
Abhinav Rana <ranaabhinav50@gmail.com>
2021-03-21 09:39:24 +0530

    Create Test.java

---
+++ tcs/Test.java
@@ -0,0 +1,18 @@
+package tcs;
+import java.util.*;
+public class Test {
+	static boolean b;
+	String Name ; 
+	int roll; 
+	public Test(int roll , String N) {
+		super();
+		this.roll = roll;
+		Name = N;
+	}
+	public static void main(String args[]) {
+		String s = "";
+		Boolean b = new Boolean(s);
+		Test obj = new Test(2,"Abhinav");
+		System.out.println(b);
+	}
+}
